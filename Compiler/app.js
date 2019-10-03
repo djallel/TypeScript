c41fa9f387ef2f9eb34a8032f2e0afadaa23d70d
TABET Djalal <b_tabet@esi.dz>
2019-10-03 22:51:24 +0200

    exercice

---
+++ Compiler/app.js
@@ -0,0 +1,6 @@
+var myName = "Max";
+var myAge = 27;
+myName = 30;
+var anything;
+anything = 12;
+// myName = 30; ceci est une erreur car myName est un string
--- Compiler/app.ts
+++ Compiler/app.ts
@@ -1,6 +1,9 @@
 let myName: string = "Max";
 let myAge: number = 27;
+myName = 30
 let anything;
 anything = 12;
 
-// myName = 30;
+// myName = 30; ceci est une erreur car myName est un string
+//important, pour myName = 30, même si c'est une erreur,
+// il va quand même généré le code dans la app.javascript (app.js)
