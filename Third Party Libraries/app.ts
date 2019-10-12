e25559ff16743bbfe6ea6a70fd2d3fbf8d2ba189
TABET Djalal <b_tabet@esi.dz>
2019-10-12 22:17:19 +0200

    Doc jquery

--- Third Party Libraries/app.ts
+++ Third Party Libraries/app.ts
@@ -1,2 +1,3 @@
 import "jQuery";
+//pour prendre en compte le $ de la ligne 3,il faut runner la commande suivante pour installer Jquery npm install --save-dev @types/jquery
 $("#app").css({"background-color": "green"});
